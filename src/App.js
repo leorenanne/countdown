4bd4ab66751d736c9c6cde73719c6cf73d3a7f3d
Leoren Tanyag <leoren.tanyag@rea-group.com>
2017-02-22 16:08:29 +1100

    Allow user input for event date
    Saves on localStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import serializeForm from 'form-serialize'
 
 class App extends Component {
   constructor(){
     super();
 
-    const today = new Date(localStorage.getItem("today"))
-    const christmas = new Date(localStorage.getItem("christmas"))
+    const today = new Date()
+    const event = new Date(localStorage.getItem("event"))
 
     this.state = {
-      difference: Math.ceil((christmas - today)/1000/60/60/24)
+      difference: Math.ceil((event - today)/1000/60/60/24)
     }
+
+    this.calculateCountdown = (event) => {
+      const values = serializeForm(event.target, { hash: true })
+
+      localStorage.setItem("event", (new Date(values.eventDate)).toJSON())
+    }
+
   }
 
   render() {
@@ -21,6 +29,18 @@ class App extends Component {
           <div id="event">Christmas</div>
           <div id="countdown">{this.state.difference}</div>
           <div id="day">DAYS</div>
+          <br/>
+
+        <form onSubmit={this.calculateCountdown}>
+          <label> Date: <input
+              type="text"
+              name="eventDate"
+              defaultValue="2020-01-01"
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+
         </div>
       </div>
 
